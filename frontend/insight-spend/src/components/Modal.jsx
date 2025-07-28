a082d58bf67703142d0b966f32c832f544aa13b2
Yuvraj Satyapal <70963261+yuvrajsatyapal@users.noreply.github.com>
2025-07-28 12:17:42 +0530

    Update Modal.jsx

--- frontend/insight-spend/src/components/Modal.jsx
+++ frontend/insight-spend/src/components/Modal.jsx
@@ -7,9 +7,7 @@ const Modal = ({children, isOpen, onClose, title}) => {
   return (
     <div className='fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50'>
         <div className="relative p-4  w-full max-w-2xl max-h-full">
-            {/* {Model Content} */}
             <div className="relative bg-white rounded-lg shadow-sm ">
-                {/* {Modal header} */}
 
                 <div className="flex items-start justify-between p-4 md:p-5 border-b rounded-t  border-gray-200">
                     <h3 className="text-lg font-medium text-gray-900 ">
@@ -35,7 +33,7 @@ const Modal = ({children, isOpen, onClose, title}) => {
                     </button>
                 </div>
 
-                {/* {Modal body} */}
+
                 <div className="p-4 md:p-5 space-y-4">
                     {children}
                 </div>
